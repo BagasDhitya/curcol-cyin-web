3bc3e6bca7b1f458aa323892ee2ba0d736919b53
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-03-04 10:11:50 +0700

    init backendless project

---
+++ src/lib/backendless.ts
@@ -0,0 +1,7 @@
+import Backendless from "backendless";
+
+const BACKENDLESS_APP_ID = "02512D93-9F9F-4345-ABAC-8F1CBFFA16E8";
+const BACKENDLESS_API_KEY = "28977B53-622B-4F5B-838E-61FD7143E7B7";
+
+Backendless.initApp(BACKENDLESS_APP_ID, BACKENDLESS_API_KEY);
+export default Backendless;
