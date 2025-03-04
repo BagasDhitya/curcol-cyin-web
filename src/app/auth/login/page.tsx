bd9c0e9006aa4d1c8a26be3fe42c7f0961851fea
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-03-04 11:42:16 +0700

    create login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,40 +3,39 @@
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { registerSchema } from "@/lib/validator/register.schema"
+import { loginSchema } from "@/lib/validator/login.schema"
 import Backendless from "@/lib/backendless"
 import { toast } from 'react-toastify'
 
-export default function Register() {
+import Cookies from 'js-cookie'
+
+export default function Login() {
 
     const router = useRouter()
     const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: zodResolver(registerSchema)
+        resolver: zodResolver(loginSchema)
     })
 
-    async function onSubmitRegister(data: { name: string, email: string, password: string }) {
+    async function onSubmitLogin(data: { email: string, password: string }) {
         try {
-            await Backendless.UserService.register(data)
-            toast.success("Registration successful")
-            router.push('/login')
+            const response: any = await Backendless.UserService.login(data.email, data.password, true)
+
+            Cookies.set("user-token", response["user-token"], { expires: 7 })
+
+            toast.success("Login successful")
+            // arahkan ke feed
         } catch (error) {
-            toast.error(`Registration failed: ${error}`)
+            toast.error(`Login failed : ${error}`)
         }
     }
 
     return (
         <div className="w-screen h-screen flex flex-col justify-center items-center">
-            <h1 className="my-5 text-blue-500 font-bold">Welcome to CurcolCyin</h1>
+            <h1 className="my-5 text-blue-500 font-bold">Login dulu gan</h1>
             <form
-                onSubmit={handleSubmit(onSubmitRegister)}
+                onSubmit={handleSubmit(onSubmitLogin)}
                 className="flex flex-col gap-y-5 items-center p-10 rounded-md shadow-sm bg-slate-100 w-1/2"
             >
-                <input
-                    {...register('name')}
-                    className="border border-slate-400 p-2 mb-2 text-black rounded-md w-1/2"
-                    placeholder="Type your name here ..."
-                    type="text" />
-                {errors.name && <p className="text-red-500 mt-2">{errors?.name?.message}</p>}
                 <input
                     {...register('email')}
                     className="border border-slate-400 p-2 mb-2 text-black rounded-md w-1/2"
@@ -52,7 +51,7 @@ export default function Register() {
                 {errors.password && <p className="text-red-500 mt-2">{errors?.password?.message}</p>}
                 <button
                     className="bg-blue-500 hover:bg-blue-600 text-white font-semibold p-4 rounded-md w-1/2"
-                    type="submit">Register</button>
+                    type="submit">Login</button>
             </form>
         </div>
     )
