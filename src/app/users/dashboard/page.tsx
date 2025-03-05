add917dc0c9f5425b07987ceed36d94eac3540bb
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-03-05 13:35:49 +0700

    create modal popup

--- src/app/users/dashboard/page.tsx
+++ src/app/users/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useRouter } from "next/navigation"
 import Backendless from "@/lib/backendless"
 import { toast } from "react-toastify"
